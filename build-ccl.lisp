9421948c65b7894d158ffc1166087d49ac7041c0
David O'Toole <blocky.io>
2014-07-29 17:36:39 -0400

    update ccl build script

--- build-ccl.lisp
+++ build-ccl.lisp
@@ -4,9 +4,9 @@
 ;;       asdf:*central-registry*)
 ;; (push #P"/home/dto/2x0ng/"
 ;;       asdf:*central-registry*)
-;; (ql:quickload '(:lispbuilder-sdl-mixer :lispbuilder-sdl-ttf :lispbuilder-sdl-image :uuid :cl-opengl :cl-fad))
+ (ql:quickload '(:lispbuilder-sdl-mixer :lispbuilder-sdl-ttf :lispbuilder-sdl-image :uuid :cl-opengl :cl-fad))
 (asdf:load-system :cypress)
-(ccl:save-application "cypress.bin" 
+(ccl:save-application "cypress.exe" 
 ;;		      :application-type :gui
 		      :prepend-kernel t
 		      :toplevel-function 'cypress:cypress)
