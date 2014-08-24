a5afb231c9ccfa5f379ea026deed2d029abe0638
David O'Toole <blocky.io>
2014-08-24 00:48:33 -0400

    fix build script

--- build-sbcl.lisp
+++ build-sbcl.lisp
@@ -7,18 +7,18 @@
 ;; (ql:quickload '(:lispbuilder-sdl-mixer :lispbuilder-sdl-ttf :lispbuilder-sdl-image :uuid :cl-opengl :cl-fad))
 (asdf:load-system :xelf)
 
-(push #p"/home/dto/cypress/" asdf:*central-registry*)
+(push #p"/home/dto/3x0ng/" asdf:*central-registry*)
 
 (setf sb-impl::*default-external-format* :utf-8)
-(asdf:oos 'asdf:load-op 'cypress)
-(sb-ext:save-lisp-and-die "cypress.exe"
+(asdf:oos 'asdf:load-op '3x0ng)
+(sb-ext:save-lisp-and-die "3x0ng.bin"
 			  :toplevel (lambda ()
 				      (sb-posix:putenv
 				       (format nil "SBCL_HOME=~A" 
 				      	       #.(sb-ext:posix-getenv "SBCL_HOME")))
 				      (setf xelf::*executable* t)
 				      (setf xelf::*suppress-warnings* t)
-				      (cypress:cypress)
+				      (3x0ng:3x0ng)
 				      0)
 			  :executable t)
 
