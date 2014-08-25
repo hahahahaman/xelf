6e11a76933b7dd8e156e7d5ecfaaac067a39a5df
David O'Toole <blocky.io>
2014-08-25 02:19:38 -0400

    fix build scripts

--- build-ccl.lisp
+++ build-ccl.lisp
@@ -4,10 +4,11 @@
 ;;       asdf:*central-registry*)
 ;; (push #P"/home/dto/2x0ng/"
 ;;       asdf:*central-registry*)
+(load "~/quicklisp/setup.lisp")
 (ql:quickload '(:lispbuilder-sdl-mixer :lispbuilder-sdl-ttf :lispbuilder-sdl-image :uuid :cl-opengl :cl-fad))
 (asdf:load-system :3x0ng)
-(ccl:save-application "3x0ng.exe" 
-		      :application-type :console
+(ccl:save-application "3x0ng.bin" 
+		      ;; :application-type :console
 		      :clear-clos-caches t
 		      :prepend-kernel t
 		      :toplevel-function '3x0ng:3x0ng)
--- build-sbcl.lisp
+++ build-sbcl.lisp
@@ -4,14 +4,14 @@
 (push (merge-pathnames "lib/" *default-pathname-defaults*)
       asdf:*central-registry*)
 
-;; (ql:quickload '(:lispbuilder-sdl-mixer :lispbuilder-sdl-ttf :lispbuilder-sdl-image :uuid :cl-opengl :cl-fad))
-(asdf:load-system :xelf)
-
+(ql:quickload '(:lispbuilder-sdl-mixer :lispbuilder-sdl-ttf :lispbuilder-sdl-image :uuid :cl-opengl :cl-fad))
 (push #p"/home/dto/3x0ng/" asdf:*central-registry*)
-
+(push #p"/home/dto/xelf/" asdf:*central-registry*)
+(ql:quickload :xelf)
 (setf sb-impl::*default-external-format* :utf-8)
 (asdf:oos 'asdf:load-op '3x0ng)
 (sb-ext:save-lisp-and-die "3x0ng.exe"
+			  :application-type :gui
 			  :toplevel (lambda ()
 				      (sb-posix:putenv
 				       (format nil "SBCL_HOME=~A" 
